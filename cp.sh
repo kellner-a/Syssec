422a8f00719d988426adb753f9ba32f7c95231eb
kellner-a <kellner.a@northeastern.edu>
2025-02-06 19:01:07 -0500

    network slides added

--- cp.sh
+++ cp.sh
@@ -4,6 +4,6 @@ read -p "challenges or slides?: " dir
 
 read -p "file you want to copy? ("file".<type>): " pdf
 
-scp -P 10001 hacker39@warhead.ccs.neu.edu:../../../usr/local/course/$dir/$pdf ~/Desktop/Syssec
+#scp -P 10001 hacker39@warhead.ccs.neu.edu:../../../usr/local/course/$dir/$pdf ~/Syssec
 
-
+scp warhead:../../../usr/local/course/$dir/$pdf ~/Syssec
